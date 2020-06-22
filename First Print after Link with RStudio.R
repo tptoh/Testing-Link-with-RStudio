7e16a0c0e0fbba7527a6f6a9a50f2016d1bb6444
Tan Toh <tptoh@yahoo.com>
2020-06-22 08:24:20 -0700

    Hope I got the right username & password

--- First Print after Link with RStudio.R
+++ First Print after Link with RStudio.R
@@ -2,4 +2,6 @@ print("This file was created within RStudio")
 print("And now it lives on GitHub"
       
 print("retrying to see if commit pull button will appear")
+
+print("forgot github username & password while trying to link")
       
